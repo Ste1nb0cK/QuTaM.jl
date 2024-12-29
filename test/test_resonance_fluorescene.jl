f26192b8c79e4c839477022c75641391bb63836f
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2024-12-29 13:22:55 -0500

    Refined the rf test, pending to fully organize the plots

--- test/test_resonance_fluorescene.jl
+++ test/test_resonance_fluorescene.jl
@@ -1,43 +1,47 @@
-
+import HypothesisTests, Distributions
+using QuTaM, LinearAlgebra, Statistics, Random, QuadGK, Test, Plots,
+    OrdinaryDiffEq
+########## Setup
 sys = QuTaM.rf_sys
 params = QuTaM.rf_params
 gamma = QuTaM.rf_gamma
 delta = QuTaM.rf_delta
 omega = QuTaM.rf_omega
-r0 = [0.0; 0.0; -1.0] # Initial Condition
+r0 = [0.0; 0.0; 1.0] # Initial Condition
 tspan = (0.0, params.tf)
 t_given = collect(LinRange(0, params.tf, 1000));
 
-# @testset "Resonance Fluorescene: Basic Operators" begin
-#        @test norm(QuTaM.rf_sys.H - H) < QuTaM.rf_EPS
-#        @test norm(QuTaM.rf_sys.Ls[1]- L) < QuTaM.rf_EPS
-#        @test norm(QuTaM.rf_sys.Heff- Heff) < QuTaM.rf_EPS
-#        @test norm(QuTaM.rf_sys.J - J) < QuTaM.rf_EPS
-
-# end
-@testset "Resonance Fluorescene: WTD" begin
 ################## Average Simulation ################3
 # Generate a set of trajectories and states
-sample_clicks = QuTaM.run_trajectories(sys, params)
+println("Sampling clicks\n")
+@time begin
+    sample_clicks = QuTaM.run_trajectories(sys, params)
+end
 ntimes = size(t_given)[1]
 sample = zeros(ComplexF64, ntimes, sys.NLEVELS, params.ntraj) # states
 
-for n in 1:params.ntraj
-    states = QuTaM.evaluate_at_t(t_given, sample_clicks[n], sys,  params.psi0)
-    for j in 1:sys.NLEVELS
-        for tn in 1:ntimes
-            sample[tn, j, n] = states[tn, j]
+println("Obtaining states between jumps\n")
+@time begin
+    for n in 1:params.ntraj
+        states = QuTaM.evaluate_at_t(t_given, sample_clicks[n], sys,  params.psi0)
+        for j in 1:sys.NLEVELS
+            for tn in 1:ntimes
+                sample[tn, j, n] = states[tn, j]
+            end
         end
     end
 end
-
 # Obtain the value of the observables
 r_sample = zeros(Float64, ntimes, 3, params.ntraj)
 sigma = [QuTaM.sigma_x, QuTaM.sigma_y, QuTaM.sigma_z]
-for j in 1:params.ntraj
-    for k in 1:3
-        for tn in 1:ntimes
+println("Calculating expectation of observables\n")
+
+@time begin
+    for j in 1:params.ntraj
+        for k in 1:3
+            for tn in 1:ntimes
                 r_sample[tn, k, j] = dot(sample[tn, :, j], sigma[k] * sample[tn, :, j])   # Drop the extra dimension
+            end
         end
     end
 end
@@ -54,8 +58,8 @@ for traj in sample_clicks
         continue
     end
 end
-# Define the analytical result for the WTD
 
+# Define the analytical result for the WTD
 struct WTD_rf <: Distributions.ContinuousUnivariateDistribution
     omega::Float64
     gamma::Float64
@@ -90,9 +94,48 @@ function Base.rand(rng::AbstractRNG, d::WTD_rf)
 end
 
 f = WTD_rf(gamma, omega) # Instance of the WTD
-f_sample = rand(f, 500) # Sample from the WTD
+println("Sampling from the analytical WTD\n")
+@time begin
+    f_sample = rand(f, 500) # Sample from the WTD
+end
 # Run a KS test
-pvalue = HypothesisTests.pvalue(HypothesisTests.ApproximateTwoSampleKSTest(tau_sample, f_sample))
+println("Runnign KS test\n")
+@time begin
+    pvalue = HypothesisTests.pvalue(HypothesisTests.ApproximateTwoSampleKSTest(tau_sample, f_sample))
+end
 rf_p0WTD = 0.2 # Minimal pvalue for accepting the null hypothesis
+
+@testset verbose=true "WTD" begin
     @test pvalue > rf_p0WTD
 end
+
+################# Visual test of the WTD #############
+WTD_analytical(tau) =  (16*gamma*omega^2)*exp(-0.5*gamma*tau) * sin(0.25*tau*sqrt(16*omega^2-gamma^2))^2/(-gamma^2+16*omega^2)
+histogram(tau_sample, normalize=:pdf, label="Sample")
+plot!(t_given, Distributions.pdf.(f, t_given), label="Analytical")
+################ Visual test of the observables
+# System of equations for RF (Source: Wiseman section 3.3.1)
+function rf_de!(dr, r, p, t)
+    gamma = QuTaM.rf_gamma
+    delta = QuTaM.rf_delta
+    omega = QuTaM.rf_omega
+    dr[1] = -0.5*gamma*r[1] - 2*delta*r[2]
+    dr[2] = 2*delta*r[1] - 0.5*gamma*r[2] - 2*omega*r[3]
+    dr[3] = 2*omega*r[2] - gamma*(r[3] + 1)
+end
+
+# Solution to the unconditional equation
+prob = ODEProblem(rf_de!, r0, tspan)
+sol = solve(prob, reltol = 1e-6, saveat = t_given);
+
+using LaTeXStrings
+p1 = plot(t_given, r_avg[:, 1], label="Unraveling", title=L"\sigma_x")
+plot!(t_given, r_sample[:,1, 1], label="Sample Trajectory")
+plot!(sol, idxs =(0, 1), label="Lindblad ")
+p2 = plot(sol, idxs =(0, 2), title=L"\sigma_y", line=:dash, legend=false)
+plot!(t_given, r_sample[:,2, 1])
+plot!(t_given, r_avg[:, 2])
+p3 = plot(sol, idxs = (0,3), title=L"\sigma_z", line=:dash, legend=false)
+plot!(t_given, r_avg[:, 3])
+plot!(t_given, r_sample[:,3, 1])
+plot(p1, p2,p3,  layout=(3, 1))
