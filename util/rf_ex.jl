77aabe0b2784f65f88cae4985276a4f02a3ea7e4
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2025-01-15 09:17:04 -0500

    Fixed rf test, and checked that the pure states tests still work after the file restructuring

--- util/rf_ex.jl
+++ util/rf_ex.jl
@@ -1,9 +1,9 @@
 ################ SETUP FOR RESONANCE FLUORESENCE ##################
 # Same thing as Radiative Damping, but now there Hamiltonian is different
 rf_EPS = 1e-5 # Tolerance for the distance respect to the Frobenious norm
-rf_delta= 1.43/2 # Detuning
-rf_omega = 1.3/2 # Rabi Frequency
-rf_gamma = 0.55
+rf_delta= 0.0 # Detuning. For comparison with the Analytic WTD this must be zero
+rf_omega = 0.5 # Rabi Frequency
+rf_gamma = 0.5
 rf_H = rf_delta*sigma_z + rf_omega*sigma_x
 
 rf_psi0 = zeros(ComplexF64, 2)
@@ -12,11 +12,11 @@ rf_psi0[1] = 1 # Initial condition
 rf_sys = System(rf_H, # Hamiltonian
 [sqrt(rf_gamma)*sigma_m]) #Jump Operators
 rf_params = SimulParameters(rf_psi0,
-    400.0, # Final time. Set very long so that all trajectories jump
+    25.0, # Final time. Set very long so that all trajectories jump
     1, # seed
     250, # Number of trajectories
     75_000, # Number of samples in the finegrid
-    4.0, # Multiplier to use in the fine grid
+    3.0, # Multiplier to use in the fine grid
     1e-3 # Tolerance for passing Dark state test
 )
 
